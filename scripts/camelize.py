d07fd383945b5d6aa866b4833c62b7ac880f3e12
Eric Fortis <efortis@gmail.com>
2026-07-24 11:16:14 -0400

    v2.1 fix caps feat on regular weight (it was not getting applied)

--- scripts/camelize.py
+++ scripts/camelize.py
@@ -6,7 +6,7 @@ from fontTools.feaLib.builder import addOpenTypeFeaturesFromString
 # This script was used for the spacing modifications. On the other hand,
 # I used Inkscape and FontForge for drawing the ligatures.
 
-OUT_DIR = Path('v2_camelana')
+OUT_DIR = Path('../v2_camelana')
 FONT_NAME = 'Camelana'
 SPACE_MULT = 4
 LEFT_PAD_FACTOR = 0.33
@@ -18,7 +18,7 @@ WEIGHTS = ['Regular', 'SemiBold', 'Italic', 'SemiBoldItalic']
 
 # Acronyms:
 #  HMTX: Horizontal Metrics Table https://learn.microsoft.com/en-us/typography/opentype/spec/hmtx
-#  LSB: Left Side Bearing
+#  LSB: Left Side Bearing (left padding)
 #  calt: Contextual Alternates https://learn.microsoft.com/en-us/typography/opentype/spec/features_ae#tag-calt
 
 def main():
@@ -27,8 +27,8 @@ def main():
 		font = TTFont(font_to_modify(weight))
 
 		new_space_width = widen_space(font, SPACE_MULT)
-		new_glyphs = inject_new_left_padded_caps_glyphs(font, int(new_space_width * LEFT_PAD_FACTOR))
-		fea = generate_feature_spec_for_using_padded_cap_when_preceding_glyph_is_lowercase(font, new_glyphs)
+		inject_new_left_padded_caps_glyphs(font, int(new_space_width * LEFT_PAD_FACTOR))
+		fea = generate_feature_spec_for_using_padded_cap_when_preceding_glyph_is_lowercase()
 		addOpenTypeFeaturesFromString(font, fea)
 
 		names = {
@@ -60,37 +60,26 @@ def inject_new_left_padded_caps_glyphs(font, padding: int):
 	glyf_table = font['glyf']
 	cmap = font.getBestCmap() # char -> glyph map
 
-	new_glyphs = {}  # original glyph name -> padded glyph name
 	for c in string.ascii_uppercase:
 		glyph_name = cmap.get(ord(c))
-		if not glyph_name or glyph_name not in horizontal_metrics:
-			continue
 		new_name = glyph_name + CUSTOM_GLYPHS_EXT
 		width, lsb = horizontal_metrics[glyph_name]
 		horizontal_metrics[new_name] = (width + padding, lsb + padding)
 		glyf_table[new_name] = glyf_table[glyph_name]
-		new_glyphs[glyph_name] = new_name
 
 	font.setGlyphOrder(glyf_table.glyphOrder)
-	return new_glyphs
 
 
-def generate_feature_spec_for_using_padded_cap_when_preceding_glyph_is_lowercase(font, new_glyphs) -> str:
-	originals = sorted(new_glyphs.keys())
-	padded = [new_glyphs[g] for g in originals]
+def generate_feature_spec_for_using_padded_cap_when_preceding_glyph_is_lowercase() -> str:
+	lowerc = ' '.join(f'\\{c}' for c in string.ascii_lowercase)
+	upperc = ' '.join(f'\\{c}' for c in string.ascii_uppercase)
+	upperp = ' '.join(f'\\{c}{CUSTOM_GLYPHS_EXT}' for c in string.ascii_uppercase)
 
-	cmap = font.getBestCmap()
-	lower_names = [cmap[ord(c)] for c in string.ascii_lowercase if ord(c) in cmap]
-
-	lower_class = ' '.join(lower_names)
-	target_class = ' '.join(originals)
-	sub_class = ' '.join(padded)
-
-	return f"""
+	return f'''
 	feature calt {{
-		sub [{lower_class}] [{target_class}]' by [{sub_class}];
+		sub [{lowerc}] [{upperc}]' by [{upperp}];
 	}} calt;
-	"""
+	'''
 
 
 if __name__ == '__main__':
